5abb8b650261cb9043a9b03aad8e4f2b3242f682
Alexsey Lutsenko <formerfor84qwedf@gmail.com>
2022-04-05 13:29:00 +0700

    deploy_5

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,8 +17,9 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,134.0.115.245,134.0.115.245:80',
-        Sanctum::currentApplicationUrlWithPort()
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
+//        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
